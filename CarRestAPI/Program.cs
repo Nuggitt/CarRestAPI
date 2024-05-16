ad22be0dcdb57c96be8ea2b5a93f1de762ace5ce
Nuggitt <phulip.nielsen@gmail.com>
2024-05-17 00:22:22 +0200

    added nothing just to see

--- CarRestAPI/Program.cs
+++ CarRestAPI/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddDbContext<CarsDbContext>(options =>
 
 
 
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
