7d80933b19b8d6d0e8e4b2484e2b7c87dff98d34
Nuggitt <Phulip.nielsen@gmail.com>
2024-05-23 10:56:58 +0200

    spændende

--- CarRestAPI/DBSecrets.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CarRestAPI
-{
-    public class DBSecrets
-    {
-        public static string ConnectionString = "Data Source=myzealandstudioserver.database.windows.net;Initial Catalog=mystudioDB;User ID=phil2629;Password=Holger1208!;Connect Timeout=60;Trust Server Certificate=True";
-
-
-    }
-}
